f9fb9c03ce0c0b75728e89df9939aee5e1adcf7d
xiaochang <xiaochang@zritc.com>
2017-01-18 20:08:31 +0800

    add bean

---
+++ app/src/main/java/com/xiaoqianchang/gankreader/bean/User.java
@@ -0,0 +1,14 @@
+package com.xiaoqianchang.gankreader.bean;
+
+/**
+ * $desc$
+ * <p>
+ * Created by Chang.Xiao on 2017/1/18.
+ *
+ * @version 1.0
+ */
+
+public class User {
+
+    private String name;
+}
